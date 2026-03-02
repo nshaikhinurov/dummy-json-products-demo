c6eeadf61c28362e5d5d8482fae1f0025f8c548a
Nail Shaikhinurov <n.shaikhinurov@gmail.com>
2026-03-02 20:47:02 +0800

    chore: configure aliases and base folder structure

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,8 @@
 import js from '@eslint/js'
-import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -13,7 +12,6 @@ export default defineConfig([
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
