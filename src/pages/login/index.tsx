9f4cef38b4a2e469cf0853cd0950695e505b77e3
Nail Shaikhinurov <n.shaikhinurov@gmail.com>
2026-03-05 21:38:19 +0800

    feat: add login form and UI components for user authentication

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,3 +1,11 @@
+import { LoginForm } from './ui/login-form'
+
 export const Login = () => {
-  return <div>Login</div>
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <LoginForm />
+      </div>
+    </div>
+  )
 }
---
+++ src/pages/login/ui/login-form.tsx
@@ -0,0 +1,144 @@
+import { AudioLines, Eye, EyeOff, Lock, User, X } from 'lucide-react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from '~/shared/ui/button'
+import { Card, CardContent, CardHeader } from '~/shared/ui/card'
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldSeparator,
+} from '~/shared/ui/field'
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from '~/shared/ui/input-group'
+
+export function LoginForm() {
+  return (
+    <Card>
+      <CardHeader>
+        <Link to="#" className="flex items-center justify-center">
+          <div className="flex size-13 items-center justify-center rounded-full bg-accent text-foreground">
+            <AudioLines className="size-8" />
+          </div>
+        </Link>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <FieldGroup>
+            <div className="flex flex-col items-center gap-1 text-center">
+              <h1 className="text-2xl font-bold">Добро пожаловать!</h1>
+              <p className="text-sm text-balance text-muted-foreground">
+                Пожалуйста, авторизуйтесь
+              </p>
+            </div>
+            <UsernameInput />
+            <PasswordInput />
+            <Field>
+              <Button type="submit">Войти</Button>
+            </Field>
+            <FieldSeparator>Или продолжить с</FieldSeparator>
+            <Field>
+              <Button variant="outline" type="button">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <path
+                    d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
+                    fill="currentColor"
+                  />
+                </svg>
+                Войти с GitHub
+              </Button>
+              <FieldDescription className="text-center">
+                Нет аккаунта?{' '}
+                <Link to="#" className="underline underline-offset-4">
+                  Создать
+                </Link>
+              </FieldDescription>
+            </Field>
+          </FieldGroup>
+        </form>
+      </CardContent>
+    </Card>
+  )
+}
+
+function UsernameInput() {
+  const [username, setUsername] = useState('')
+
+  return (
+    <Field>
+      <FieldLabel htmlFor="username">Логин</FieldLabel>
+      <InputGroup>
+        <InputGroupInput
+          id="username"
+          placeholder="Введите логин"
+          required
+          autoComplete="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <InputGroupAddon>
+          <User />
+        </InputGroupAddon>
+        {username && (
+          <InputGroupAddon align={'inline-end'}>
+            <Button
+              type="button"
+              variant="ghost"
+              size="icon-sm"
+              onClick={() => setUsername('')}
+              aria-label="Clear username"
+              className={'hover:bg-transparent'}
+            >
+              <X />
+            </Button>
+          </InputGroupAddon>
+        )}
+      </InputGroup>
+    </Field>
+  )
+}
+
+function PasswordInput() {
+  const [showPassword, setShowPassword] = useState(false)
+
+  return (
+    <Field>
+      <div className="flex items-center">
+        <FieldLabel htmlFor="password">Пароль</FieldLabel>
+        <Link
+          to="#"
+          className="ml-auto text-sm underline-offset-4 hover:underline"
+        >
+          Забыли пароль?
+        </Link>
+      </div>
+      <InputGroup>
+        <InputGroupInput
+          id="password"
+          type={showPassword ? 'text' : 'password'}
+          required
+          autoComplete="current-password"
+        />
+        <InputGroupAddon>
+          <Lock />
+        </InputGroupAddon>
+        <InputGroupAddon align={'inline-end'}>
+          <Button
+            type="button"
+            variant="ghost"
+            size="icon-sm"
+            onClick={() => setShowPassword((prev) => !prev)}
+            aria-label={showPassword ? 'Hide password' : 'Show password'}
+            className={'hover:bg-transparent'}
+          >
+            {showPassword ? <EyeOff /> : <Eye />}
+          </Button>
+        </InputGroupAddon>
+      </InputGroup>
+    </Field>
+  )
+}
